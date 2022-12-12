a368bdc178f862f6e9bbe451f4d2cc51bf906ae1
bubble-code <57643296+bubble-code@users.noreply.github.com>
2022-12-12 08:14:14 +0100

    's'

--- skillUp.py
+++ skillUp.py
@@ -1,36 +1,53 @@
-def maximumPerimeterTriangle(sticks):
-    sticks.sort()
-    for i in range(len(sticks)-3, -1, -1):
-        if sticks[i] + sticks[i+1] > sticks[i+2]:
-            return [sticks[i], sticks[i+1], sticks[i+2]]
-    return [-1]
-# ===============================
+# def maximumPerimeterTriangle(sticks):
+#     sticks.sort()
+#     for i in range(len(sticks)-3, -1, -1):
+#         if sticks[i] + sticks[i+1] > sticks[i+2]:
+#             return [sticks[i], sticks[i+1], sticks[i+2]]
+#     return [-1]
+# # ===============================
 
 
-def findZigZagSequence(a, n):
-    a.sort()
-    mid = int(n / 2)
-    a[mid], a[n-1] = a[n-1], a[mid]
+# def findZigZagSequence(a, n):
+#     a.sort()
+#     mid = int(n / 2)
+#     a[mid], a[n-1] = a[n-1], a[mid]
 
-    st = mid + 1
-    ed = n - 2
-    while (st <= ed):
-        a[st], a[ed] = a[ed], a[st]
-        st = st + 1
-        ed = ed - 1
+#     st = mid + 1
+#     ed = n - 2
+#     while (st <= ed):
+#         a[st], a[ed] = a[ed], a[st]
+#         st = st + 1
+#         ed = ed - 1
 
-    for i in range(n):
-        if i == n-1:
-            print(a[i])
-        else:
-            print(a[i], end=' ')
-    return
+#     for i in range(n):
+#         if i == n-1:
+#             print(a[i])
+#         else:
+#             print(a[i], end=' ')
+#     return
 
 
-test_cases = int(input())
-for cs in range(test_cases):
-    n = int(input())
-    a = list(map(int, input().split()))
-    findZigZagSequence(a, n)
-    
-findZigZagSequence(list([1, 2, 3, 4, 5, 6, 7]), 7)
+# test_cases = int(input())
+# for cs in range(test_cases):
+#     n = int(input())
+#     a = list(map(int, input().split()))
+#     findZigZagSequence(a, n)
+
+# findZigZagSequence(list([1, 2, 3, 4, 5, 6, 7]), 7)
+
+import time
+
+
+def progress_bar(step:int, total:int, len:int)->str:
+    frac = step/total
+    completed = int(frac*len)
+    rest = len - completed
+    return f"[{'#'*completed}{'-'*rest}]{frac:.0%}"
+
+
+n = 30
+l = 10
+
+for i in range(n+1):
+    time.sleep(0.1)
+    print(progress_bar(i, 30, l), end='\r')
